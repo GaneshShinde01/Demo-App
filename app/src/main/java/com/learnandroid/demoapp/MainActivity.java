25491db3f1e35cd97907dfb3f2b56e09200df8d4
ganesh shinde <ganeshss978081@gmail.com>
2025-01-31 10:55:30 +0530

    main activity ammended

--- app/src/main/java/com/learnandroid/demoapp/MainActivity.java
+++ app/src/main/java/com/learnandroid/demoapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
 
