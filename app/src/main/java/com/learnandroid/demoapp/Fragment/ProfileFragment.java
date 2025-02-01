4e30ff31264c650036611fa48222962d7c7b2f30
ganesh shinde <ganeshss978081@gmail.com>
2025-02-01 14:39:32 +0530

    profile fragment amended

--- app/src/main/java/com/learnandroid/demoapp/Fragment/ProfileFragment.java
+++ app/src/main/java/com/learnandroid/demoapp/Fragment/ProfileFragment.java
@@ -25,7 +25,10 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        //return inflater.inflate(R.layout.fragment_profile, container, false);
+        binding = FragmentProfileBinding.inflate(getLayoutInflater());
+
+        return binding.getRoot();
     }
 
     @Override
