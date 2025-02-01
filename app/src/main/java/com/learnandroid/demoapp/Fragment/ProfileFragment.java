257557c9cbed7186365a07d030e4754e44e5f65b
ganesh shinde <ganeshss978081@gmail.com>
2025-02-01 14:35:20 +0530

    profile fragment amended

--- app/src/main/java/com/learnandroid/demoapp/Fragment/ProfileFragment.java
+++ app/src/main/java/com/learnandroid/demoapp/Fragment/ProfileFragment.java
@@ -18,24 +18,6 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
