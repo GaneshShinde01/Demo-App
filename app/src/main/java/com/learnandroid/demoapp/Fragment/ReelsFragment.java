ef062c3707173521459ff523e1bfd46107e26dfd
ganesh shinde <ganeshss978081@gmail.com>
2025-02-04 21:35:03 +0530

    Reels fragment amended

--- app/src/main/java/com/learnandroid/demoapp/Fragment/ReelsFragment.java
+++ app/src/main/java/com/learnandroid/demoapp/Fragment/ReelsFragment.java
@@ -10,21 +10,8 @@ import android.view.ViewGroup;
 
 import com.learnandroid.demoapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReelsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ReelsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ReelsFragment() {
         // Required empty public constructor
