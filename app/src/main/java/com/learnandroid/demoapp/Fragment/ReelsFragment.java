9271b54c9678a2c5695ad78d581835e188914fcf
ganesh shinde <ganeshss978081@gmail.com>
2025-02-04 21:35:45 +0530

    Reels fragment amended

--- app/src/main/java/com/learnandroid/demoapp/Fragment/ReelsFragment.java
+++ app/src/main/java/com/learnandroid/demoapp/Fragment/ReelsFragment.java
@@ -19,15 +19,6 @@ public class ReelsFragment extends Fragment {
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
