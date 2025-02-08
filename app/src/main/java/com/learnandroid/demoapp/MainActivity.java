facaa2b503b959a5a142e22b7feaa79a2ff92ce9
ganesh shinde <ganeshss978081@gmail.com>
2025-02-08 10:49:33 +0530

    bottom nav added

--- app/src/main/java/com/learnandroid/demoapp/MainActivity.java
+++ app/src/main/java/com/learnandroid/demoapp/MainActivity.java
@@ -1,13 +1,16 @@
 package com.learnandroid.demoapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.learnandroid.demoapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +24,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        binding.bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 
     @Override
